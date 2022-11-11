36720cfd4d1d26d2e4422929b6272053da6eb61e
HadziqM <50319538+HadziqM@users.noreply.github.com>
2022-11-11 19:53:24 +0700

    comments

--- components/comment_card.tsx
+++ components/comment_card.tsx
@@ -7,15 +7,15 @@ interface Props {
 
 export default function CommentCard(props: Props) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col bg-gray-200">
       <div className="flex items-center">
-        <h2 className="mr-4">{props.name}</h2>
         <img
           src={props.url}
           alt={props.name}
-          className="border-gold border h-4 w-4 rounded-full"
+          className="border-gold border h-8 w-8 rounded-full"
         />
-        <p className="ml-8">{new Date(props.date).toString()}</p>
+        <h2 className="mr-2 font-bold">{props.name}</h2>
+        <p className="ml-4">{new Date(props.date).toString()}</p>
       </div>
       <p>{props.comment}</p>
     </div>
